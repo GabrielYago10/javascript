278e0bf2c4d423126ac818d4a543aca32e1a8232
gabriel yago <ygabriel.yago@gmail.com>
2024-09-17 22:04:07 -0300

    estudo das variaveis

---
+++ Fundamentos/variaveis.js
@@ -0,0 +1,34 @@
+# variaveis.js #
+
+/**
+* Estudo de constante e variavel
+* @author Gabriel Yago
+*/
+ 
+console.clear()
+
+// constante
+const pi = 3.14
+ 
+//variaveis
+let nome 
+let idade 
+let vip
+ 
+nome = "Gabriel Yago"
+console.log(typeof(nome))
+console.log(nome)
+ 
+idade= "20"
+console.log(typeof(idade))
+console.log(idade)
+ 
+vip = true 
+console.log(typeof(vip))
+console.log(loggi)
+
+console.log(pi)
+//pi = 3.17
+
+nome = "Gabriel Yago"
+console.log(nome)
