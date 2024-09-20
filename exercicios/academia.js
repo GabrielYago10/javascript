d3e216ab9e8e14c5d5b2856f054767da98d77875
gabriel yago <ygabriel.yago@gmail.com>
2024-09-19 22:20:13 -0300

    estudo variaveis

---
+++ exercicios/academia.js
@@ -0,0 +1,27 @@
+/**
+ *  Ficha de um alino de academia 
+ * @author Gabriel Yago  
+ */
+
+// variaveis 
+let nome 
+let idade
+let peso 
+let altura 
+let vip
+
+console.clear()
+nome = "Gabriel Yago"
+idade = 20
+peso = 53
+altura = 1.75
+vip = true
+
+// saida 
+console.log("Ficha do aluno")
+console.log("_____________________")
+console.log(`Nome: ${nome}`)
+console.log( `Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura} `)
+console.log(`vip: ${vip}`)
